f7734e245fee970f7a49a7f7ab023e86beabd1fe
Bence <bence.dudas20@gmail.com>
2023-04-03 11:24:47 +0200

    paddingForBatches

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ def get_track(pth:str = DEF_FOLD_PATH)->pd.DataFrame:
     hit = pd.DataFrame(np.load(pth[0]))
     ahit = pd.DataFrame(np.load(pth[1],allow_pickle=True)) 
     hit['Layer'] =  2*(hit['volumeID[2]'])+hit['volumeID[3]']
-    hit = hit[["trackID","eventID","parentID","posX","posY","posZ","Layer"] ]
+    hit = hit[["trackID","eventID","parentID","posX","posY","posZ","Layer","edep"] ]
     hit = hit[hit.parentID==0]
 
     ahit = ahit[["TrackID","EventID","ParentID","dX","dY","dZ","Ekine"]]
@@ -20,3 +20,18 @@ def get_track(pth:str = DEF_FOLD_PATH)->pd.DataFrame:
     tracks.set_index(["EventID","TrackID"],inplace=True)
     tracks.drop(["trackID","eventID","Layer"],axis=1,inplace=True)
     return tracks
+
+
+MAX_LAYER = 25
+
+def padBatch(df):
+    trL = df.posZ.max()+1
+    numParticles = int(df.index.size/trL)
+    
+    padding = tf.constant([[0,MAX_LAYER-trL],[0,0]])
+    res =  tf.pad(df[0*trL:1*trL],padding,"CONSTANT")
+    for pidx in range(1,numParticles-1):
+        padding = tf.constant([[0,MAX_LAYER-trL],[0,0]])
+        r1 = tf.pad(df[pidx*trL:(pidx+1)*trL],padding,"CONSTANT")
+        res = tf.concat([res,r1],0) 
+    return res 
