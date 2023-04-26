18bc686af20b4b2c514d787d56eaeb9bb184eacb
Dudgit <bence.dudas20@gmail.com>
2023-04-26 15:18:14 +0000

    hyp updated

--- src/hyperparams.py
+++ src/hyperparams.py
@@ -1,14 +1,17 @@
 # hyperparameters
 batch_size = 64 # how many independent sequences will we process in parallel?
 block_size = 256 # what is the maximum context length for predictions?
+
 max_iters = 5000
 eval_interval = 500
-learning_rate = 3e-3
 eval_iters = 200
+learning_rate = 3e-3
+
 n_embd = 8
 n_head = 6
 n_layer = 6
 dropout = 0.2
+
 EPOCHS = 100
 MAX_LAYER = 45
 train_steps = 20
